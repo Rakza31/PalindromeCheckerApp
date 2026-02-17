3347ce4e5f87d9f9df61541c53e60eed9cdffe56
Rakza31 <s.raksaruba@gmail.com>
2026-02-17 11:14:32 +0530

    Updated to UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,43 @@
-public class PalindromeCheckerApp{
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
 
+        // Original string
+        String input = "madam";
 
+        // Create Queue (FIFO)
+        Queue<Character> queue = new LinkedList<>();
+
+        // Create Stack (LIFO)
+        Stack<Character> stack = new Stack<>();
+
+        // Add characters to both Queue and Stack
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            queue.add(ch);   // Enqueue
+            stack.push(ch);  // Push
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue (FIFO) and pop (LIFO)
+        while (!queue.isEmpty()) {
+
+            char fromQueue = queue.remove(); // Dequeue
+            char fromStack = stack.pop();    // Pop
+
+            if (fromQueue != fromStack) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input text : " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
