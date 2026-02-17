c93002c17f3ce7893cbb5d392fa2961ed47369ea
Rakza31 <s.raksaruba@gmail.com>
2026-02-17 10:40:07 +0530

    added a welcome msg

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
+        System.out.println("Welcome to Palindrome Checker App");
 
 
     }
