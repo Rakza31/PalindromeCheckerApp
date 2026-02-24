14083145d78363c7596e99ffd70a5de9bd719b68
Raksa <s.raksaruba@gmail.com>
2026-02-24 10:48:17 +0530

    Refactor palindrome checker using LinkedList

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,35 +1,18 @@
-import java.util.Stack;
-
-public class PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
-        // Original string
-        String input = "madam";
-
-        // Create stack
-        Stack<Character> stack = new Stack<>();
-
-        // Push characters into stack
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+public class UseCase8PalindromeCheckerApp{
+    public static void main(String[] args){
+        String word="level";
+        LinkedList<Character> list=new LinkedList<>();
+        for(char c:word.toCharArray()){
+            list.add(c);
         }
-
-        boolean isPalindrome = true;
-
-        // Pop characters and compare
-        for (int i = 0; i < input.length(); i++) {
-
-            char poppedChar = stack.pop();
-
-            if (input.charAt(i) != poppedChar) {
-                isPalindrome = false;
+        boolean palindrome=true;
+        while(list.size()>1){
+            if (list.removeFirst()!=list.removeLast()) {
+                palindrome=false;
                 break;
             }
         }
-
-        // Display result
-        System.out.println("Input text : " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Input text: "+word);
+        System.out.println("Is it a palindrome?: "+palindrome);
     }
 }
